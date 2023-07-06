783585039b91970fd68af8c3fb71d408be8a590e
Obruch Polina <obruchspb@yandex.ru>
2023-07-06 16:11:48 +0300

    feat: реализовать работу с Request через контейнер

---
+++ gateway/src/main/java/ru/practicum/shareit/request/RequestClient.java
@@ -0,0 +1,46 @@
+package ru.practicum.shareit.request;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.shareit.client.BaseClient;
+import ru.practicum.shareit.request.dto.RequestAddDto;
+
+import java.util.Map;
+
+@Service
+public class RequestClient extends BaseClient {
+    private static final String API_PREFIX = "/requests";
+
+    public RequestClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
+                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                        .build()
+        );
+    }
+
+    public ResponseEntity<Object> add(Long userId, RequestAddDto dto) {
+        return post("/", userId, dto);
+    }
+
+    public ResponseEntity<Object> getAllRequestByOwnerId(Long userId) {
+        return get("/", userId);
+    }
+
+    public ResponseEntity<Object> getAllRequest(Long userId, Integer from, Integer size) {
+        Map<String, Object> parameters = Map.of(
+                "from", from,
+                "size", size
+        );
+        return get("/all?from={from}&size={size}", userId, parameters);
+    }
+
+    public ResponseEntity<Object> getByRequestIdWithItem(Long requestId, Long userId) {
+        return get("/" + requestId, userId);
+    }
+}
--- gateway/src/main/java/ru/practicum/shareit/request/RequestController.java
+++ gateway/src/main/java/ru/practicum/shareit/request/RequestController.java
@@ -1,4 +1,56 @@
 package ru.practicum.shareit.request;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.practicum.shareit.request.dto.RequestAddDto;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+
+@Slf4j
+@Validated
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/requests")
 public class RequestController {
+    private static final String USER_ID_HEADER = "X-Sharer-User-Id";
+    private final RequestClient requestClient;
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestHeader(name = USER_ID_HEADER) Long userId,
+                                      @Valid @RequestBody RequestAddDto dto) {
+        log.info("Запрос на создание запроса - сервер gateway");
+        return requestClient.add(userId, dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getAllRequestByOwnerId(@RequestHeader(name = USER_ID_HEADER) Long userId) {
+        log.info("Запрос на выдачу списка запросов пользователя - сервер gateway");
+        return requestClient.getAllRequestByOwnerId(userId);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Object> getAllRequest(@RequestHeader(name = USER_ID_HEADER) Long userId,
+                                                @PositiveOrZero @RequestParam(defaultValue = "0") Integer from,
+                                                @Positive @RequestParam(defaultValue = "20") Integer size) {
+        log.info("Запрос на выдачу списка всех запросов - сервер gateway");
+        return requestClient.getAllRequest(userId, from, size);
+    }
+
+    @GetMapping("/{requestId}")
+    public ResponseEntity<Object> getByRequestIdWithItem(@PathVariable Long requestId, @RequestHeader(name = USER_ID_HEADER) Long userId) {
+        log.info("Запрос на выдачу запроса - сервер gateway");
+        return requestClient.getByRequestIdWithItem(requestId, userId);
+    }
 }
